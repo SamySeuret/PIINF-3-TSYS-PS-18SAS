99df76bc6b66d691418d1b01f9bfbcc72585cd6e
Samy Seuret <samy.seuret@ceff.ch>
2022-12-20 15:32:06 +0100

    éteindre le PC à distance

--- projet_powershell.ps1
+++ projet_powershell.ps1
@@ -1,1 +1,11 @@
-"test"
+Import-Module ActiveDirectory
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$chemin = "C:\Users\Administrator\Desktop\pc.csv"
+
+Get-ADComputer -Filter * | Where-Object {$_.name -match "CEFF"} | Select -Property Name | Export-Csv $chemin
+
+[System.Windows.Forms.MessageBox]::Show("Fichier pc.csv crée sur le Bureau", "Information" , 0, 64)
+
+$liste = Import-Csv "C:\Users\Administrator\Desktop\pc.csv"
+ForEach ($item in $liste) {Stop-Computer -ComputerName $item.name -Force -Credential "projet.local\"}
