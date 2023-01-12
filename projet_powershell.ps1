a4013cf279b3a2c766eaed6edbba2aa4ae043970
Samy Seuret <samy.seuret@ceff.ch>
2023-01-12 11:05:26 +0100

    projet fini

--- projet_powershell.ps1
+++ projet_powershell.ps1
@@ -1,3 +1,11 @@
+# Ce script est un menu de projet PowerShell qui permet aux utilisateurs de sélectionner une option pour éteindre ou redémarrer des ordinateurs à distance à l'aide de l'Active Directory.
+
+# La fonction DisplayMenu affiche un menu à l'utilisateur avec quatre options:
+# 1) Eteindre PC - Eteint les ordinateurs à distance en utilisant le nom AD
+# 2) Redémarrer PC - Sélectionne une OU de l'Active Directory et redémarre les ordinateurs de cette OU
+# 3) Eteindre class - Sélectionne une classe de l'Active Directory et éteint les ordinateurs de cette classe
+# 4) Exit - Quitte le script
+
 function DisplayMenu {
     Clear-Host
     Write-host @"
@@ -18,14 +26,14 @@ function DisplayMenu {
     Switch ($MENU) {
     
         1 {
-            #éteindre les PC à distance avec le nom AD
-            #-------------------------------------------------------------------------------------------------------------------------
+            # Eteindre les PC à distance avec le nom AD
+            
             Import-Module ActiveDirectory
             [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     
             $chemin = "C:\Users\Administrator\Desktop\pc.csv"
     
-            Get-ADComputer -Filter * | Where-Object { $_.name -match "CEFF" } | Select -Property Name | Export-Csv $chemin
+            Get-ADComputer -Filter * | Where-Object { $_.name -match "CEFF" } | Select-Object Name | Export-Csv $chemin
     
             [System.Windows.Forms.MessageBox]::Show("Fichier pc.csv crée sur le Bureau", "Information" , 0, 64)
     
@@ -33,7 +41,7 @@ function DisplayMenu {
     
             $credential = Get-Credential -Credential "projet.local\administrator"
     
-            ForEach ($item in $liste) { Stop-Computer -ComputerName $item.name -Force $credential }
+            ForEach ($item in $liste) { Stop-Computer -ComputerName $item.Name -Force $credential }
         }
     
         2 {
@@ -45,53 +53,43 @@ function DisplayMenu {
     
             # Affichez la liste des ordinateurs de l'OU sélectionnée
             $ComputerToReboot = $Computers | Out-GridView -Title 'Sélectionnez l''ordinateur à redémarrer' -PassThru
-    
-            # Redémarrez l'ordinateur sélectionné à distance
+	        # Redémarrez l'ordinateur sélectionné à distance
             Restart-Computer -ComputerName $ComputerToReboot -Force
         }
     
         3 {
+            # Eteindre class
+            Write-Host "Choisir une classe : "
+            Write-Host "1) Class1"
+            Write-Host "2) Class2"
     
-            Write-Host @"
-    +=======================+
-    |     Choisir une OU    |
-    +=======================+
-    |                       |
-    |      1) Class1        |
-    |      2) Class2        |
-    |                       |
-    +=======================+
+            $choix = Read-Host "Entrez le numéro de la classe : "
     
-"@
+            Switch($choix) {
+                1 {
+                    $Computers = Get-ADComputer -SearchBase "OU=class1,DC=projet,DC=local" -Filter * 
+                    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
+                }
+                2 {
+                    $Computers = Get-ADComputer -SearchBase "OU=class2,DC=projet,DC=local" -Filter * 
+                    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
+                }
+                default {
+                    Write-Host "Option non valide"
+                }
+            }
+        }
     
-    $choix = Read-Host "choisir une OU : "
+        4 {
+            # Exit
+            Exit
+        }
     
-    Switch($choix)
-    {
-    
-    1{
-    $Computers = Get-ADComputer -SearchBase "OU=class1,DC=projet,DC=local" -Filter * #doit etre loguer pour éteindre le pc et redémarrer à chaque fois le serveur ad pour re faire le shutdown
-    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
+        default {
+            Write-Host "Option non valide"
+        }
     }
-    
-    
-    2{
-    $Computers = Get-ADComputer -SearchBase "OU=class2,DC=projet,DC=local" -Filter *
-    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
-    }
-    }
-    }
-    
-    4{
-    Write-Host "Bye"
-    break
-    }
-    
-    default{
-    Write-Host "Option non valide"
-    Start-Sleep -Seconds 2
-    displayMenu
-    }
-    }
-    }
-    DisplayMenu
+}
+
+# Appel de la fonction DisplayMenu pour afficher le menu
+DisplayMenu
