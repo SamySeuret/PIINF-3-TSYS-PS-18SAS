3803ce38e9b067e71b04ec04cbfc7164948b7c59
Samy Seuret <samy.seuret@ceff.ch>
2023-01-12 10:06:00 +0100

    projet fini

--- projet_powershell.ps1
+++ projet_powershell.ps1
@@ -2,12 +2,12 @@ function DisplayMenu {
     Clear-Host
     Write-host @"
     +==============================+
-    |  PROJET POWERSHELL MENU      |
+    |    PROJET POWERSHELL MENU    |
     +==============================+
     |                              |
     |        1) Eteindre PC        |
     |        2) Redémarrer PC      |
-    |        3) Wake on LAN        |
+    |        3) Eteindre class     |
     |        4) Exit               |
     |                              |
     +==============================+
@@ -15,67 +15,73 @@ function DisplayMenu {
 "@
     
     $MENU = Read-Host "OPTION"
-    Switch ($MENU)
-    {
+    Switch ($MENU) {
     
-    1{
-    #éteindre les PC à distance avec le nom AD
-    #-------------------------------------------------------------------------------------------------------------------------
-    Import-Module ActiveDirectory
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+        1 {
+            #éteindre les PC à distance avec le nom AD
+            #-------------------------------------------------------------------------------------------------------------------------
+            Import-Module ActiveDirectory
+            [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
     
-    $chemin = "C:\Users\Administrator\Desktop\pc.csv"
+            $chemin = "C:\Users\Administrator\Desktop\pc.csv"
     
-    Get-ADComputer -Filter * | Where-Object {$_.name -match "CEFF"} | Select -Property Name | Export-Csv $chemin
+            Get-ADComputer -Filter * | Where-Object { $_.name -match "CEFF" } | Select -Property Name | Export-Csv $chemin
     
-    [System.Windows.Forms.MessageBox]::Show("Fichier pc.csv crée sur le Bureau", "Information" , 0, 64)
+            [System.Windows.Forms.MessageBox]::Show("Fichier pc.csv crée sur le Bureau", "Information" , 0, 64)
     
-    $liste = Import-Csv "C:\Users\Administrator\Desktop\pc.csv"
+            $liste = Import-Csv "C:\Users\Administrator\Desktop\pc.csv"
     
-    $credential = Get-Credential -Credential "projet.local\administrator"
+            $credential = Get-Credential -Credential "projet.local\administrator"
     
-    ForEach ($item in $liste) {Stop-Computer -ComputerName $item.name -Force $credential }
+            ForEach ($item in $liste) { Stop-Computer -ComputerName $item.name -Force $credential }
+        }
     
-    DisplayMenu
-    }
+        2 {
+            # Sélectionnez une OU à partir de l'Active Directory
+            $OU = Get-ADOrganizationalUnit -Filter * | Out-GridView -Title 'Sélectionnez une OU' -PassThru
     
-    2{
-    Import-Module ActiveDirectory
-    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+            # Récupérez tous les ordinateurs de l'OU sélectionnée
+            $Computers = Get-ADComputer -SearchBase $OU.DistinguishedName -Filter * | Select-Object -ExpandProperty Name
     
-    $chemin = "C:\Users\Administrator\Desktop\pc.csv"
+            # Affichez la liste des ordinateurs de l'OU sélectionnée
+            $ComputerToReboot = $Computers | Out-GridView -Title 'Sélectionnez l''ordinateur à redémarrer' -PassThru
     
-    Get-ADComputer -Filter * | Where-Object {$_.name -match "CEFF"} | Select -Property Name | Export-Csv $chemin
+            # Redémarrez l'ordinateur sélectionné à distance
+            Restart-Computer -ComputerName $ComputerToReboot -Force
+        }
     
-    [System.Windows.Forms.MessageBox]::Show("Fichier pc.csv crée sur le Bureau", "Information" , 0, 64)
+        3 {
     
-    $liste = Import-Csv "C:\Users\Administrator\Desktop\pc.csv"
+            Write-Host @"
+    +=======================+
+    |     Choisir une OU    |
+    +=======================+
+    |                       |
+    |      1) Class1        |
+    |      2) Class2        |
+    |                       |
+    +=======================+
     
-    $credential = Get-Credential -Credential "projet.local\administrator"
-    
-    ForEach ($item in $liste) {Restart-Computer -ComputerName $item.name -Force $credential }
-    }
-    
-    3{
-    Write-Host "choisire une OU à arreter"
-    Write-Host "1) class1"
-    Write-Host "2) class2"
+"@
     
     $choix = Read-Host "choisir une OU : "
     
     Switch($choix)
     {
+    
     1{
-    $Computers = Get-ADComputer -SearchBase "OU=class1,DC=projet,DC=local" -Filter * #doit etre loguer pour éteindre le pc
-    Invoke-Command -ComputerName $computers.Name -ScriptBlock {shutdown /s /t 0}
+    $Computers = Get-ADComputer -SearchBase "OU=class1,DC=projet,DC=local" -Filter * #doit etre loguer pour éteindre le pc et redémarrer à chaque fois le serveur ad pour re faire le shutdown
+    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
     }
+    
     
     2{
     $Computers = Get-ADComputer -SearchBase "OU=class2,DC=projet,DC=local" -Filter *
-    Invoke-Command -ComputerName $computers.Name -ScriptBlock {shutdown /s /t 0}
+    Invoke-Command -ComputerName $Computers.Name -ScriptBlock {shutdown /s /t 0}
     }
     }
     }
+    
     4{
     Write-Host "Bye"
     break
